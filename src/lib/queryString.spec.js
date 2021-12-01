e7faacd7a97347b85abbb2b5f820da23401717ac
Paulo Roberto <paulo.pereira.dev@gmail.com>
2021-12-01 08:38:52 -0300

    feat: more queryString tests

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -9,4 +9,27 @@ describe('Object to query string', () => {
 
     expect(queryString(obj)).toBe('name=Paulo&profession=developer')
   })
+
+  it('should create a valid query string even when a array is passed as a value', () => {
+    const obj = {
+      name: 'Paulo',
+      abilities: ['JS', 'TDD']
+    }
+
+    expect(queryString(obj)).toBe('name=Paulo&abilities=JS,TDD')
+  })
+
+  it('should throw an error when an object is passed as value', () => {
+    const obj = {
+      name: 'Paulo',
+      abilities: {
+        first: 'JS',
+        second: 'TDD'
+      }
+    }
+
+    expect(() => {
+      queryString(obj)
+    }).toThrowError()
+  })
 })
